33e404134c072f16f9a1caa86fd84be0fcf0fd18
musfique113 <musfique113@gmail.com>
2023-04-29 23:27:06 +0600

    api problem solved

--- lib/Activity/home.dart
+++ lib/Activity/home.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
-
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -12,36 +11,25 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
-  void getData() async{
-    Response response = await get(Uri.parse("https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=0dadc0ec41869d72cab605ebf70d5c96}"));
-    //Response response =await get("https://mytimeapi.herokuapp.com/time/Asia/Kolkata" as Uri);
+  void getData() async {
+    Response response = await get(Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=0dadc0ec41869d72cab605ebf70d5c96"));
     Map data = jsonDecode(response.body);
-    print(data);
+    //print(data);
+    Map temp_data = data['main'];
+    var temp = temp_data['temp'];
+    print(temp);
+
+    // // this api form weatherApi.com
+    // Response response = await get(Uri.parse(
+    //     "https://api.weatherapi.com/v1/current.json?key=697dbef7df384b00bd5165247232804&q=Dhaka&aqi=no}"));
+    // Map data = jsonDecode(response.body);
+    // Map temp_data = data['current'];
+    // var locationName = temp_data['temp_c'];
+    // print(locationName);
   }
-  // var ff;
-  //
-  // void getData() async{
-  //   await Future.delayed(Duration(seconds: 5),(){
-  //     ff = "Test f";
-  //   });
-  //   print(ff);
-  // }
-  //
-  // void showData() async{
-  //   getData();
-  //   print(ff);
-  // }
 
-  // void timer(){
-  //   //timer for 5 second
-  //   Future.delayed(Duration(seconds: 5),(){
-  //     print("Arlame is going on");
-  //   });
-  //   print("Do onther stuf");
-  // }
-
-  int counter = 1 ;
+  int counter = 1;
   @override
   void initState() {
     getData();
@@ -58,30 +46,30 @@ class _HomeState extends State<Home> {
     super.setState(fn);
     print("Set state called");
   }
+
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
     print("Disposed");
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Activity"),
       ),
-      body:
-          Column(
-            children: <Widget>[
-              FloatingActionButton(
-                  onPressed: () => setState(() {
-                    counter += 1;
-                  }),
-              ),
-              Text("$counter")
-            ],
+      body: Column(
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () => setState(() {
+              counter += 1;
+            }),
           ),
+          Text("$counter")
+        ],
+      ),
     );
   }
 }
